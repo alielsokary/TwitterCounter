c85ff481adc5fb7debd3bd5bd64f2b9cd2b87619
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-05-02 01:01:33 +0300

    Bind CharacterCount, RemainingCount to ViewModel

--- TwitterCounter/Scene/View/TwitterCounterViewController.swift
+++ TwitterCounter/Scene/View/TwitterCounterViewController.swift
@@ -6,10 +6,10 @@
 //
 
 import UIKit
-
+import Combine
 
 class TwitterCounterViewController: UIViewController {
-    
+    // MARK: Properties
     @IBOutlet private(set) weak var charactersTypedLabel: UILabel!
     @IBOutlet private(set) weak var charactersRemainingLabel: UILabel!
     
@@ -20,26 +20,46 @@ class TwitterCounterViewController: UIViewController {
     
     @IBOutlet private(set) weak var postTweetButton: UIButton!
     
+    private var viewModel = TwitterCounterViewModel()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        tweetTextView.delegate = self
-        updateCharacterCount()
-    }
-    
-    func updateCharacterCount() {
-        let tweetText = tweetTextView.text ?? ""
-        let characterCount = TwitterText.lengthOf(tweet: tweetText)
-        let remainingCount = 280 - characterCount
-        charactersTypedLabel.text = "\(characterCount)\\280"
-        charactersRemainingLabel.text = "\(remainingCount)"
+        configureTextView()
+        setupBindings()
     }
 }
 
-extension TwitterCounterViewController: UITextViewDelegate {
-    
-    func textViewDidChange(_ textView: UITextView) {
-        updateCharacterCount()
+// MARK: - Configurations
+
+private extension TwitterCounterViewController {
+    func configureTextView() {
+        tweetTextView.delegate = self
     }
+    
+    func setupBindings() {
+        viewModel.$characterCount
+            .map { "\($0)/280" }
+            .assign(to: \.text, on: charactersTypedLabel)
+            .store(in: &cancellables)
+
+        viewModel.$remainingCount
+            .map { "\($0)" }
+            .assign(to: \.text, on: charactersRemainingLabel)
+            .store(in: &cancellables)
+
+        NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification, object: tweetTextView)
+            .compactMap { ($0.object as? UITextView)?.text }
+            .assign(to: \.tweetText, on: viewModel)
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - UITextViewDelegate
+
+extension TwitterCounterViewController: UITextViewDelegate {
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let currentText = textView.text as NSString
