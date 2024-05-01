e3bfe687582cadeba7b84534d86ac1bbbdeb760a
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-05-01 23:46:55 +0300

    Add first implementation of business logic

--- TwitterCounter/Scene/View/TwitterCounterViewController.swift
+++ TwitterCounter/Scene/View/TwitterCounterViewController.swift
@@ -9,22 +9,42 @@ import UIKit
 
 
 class TwitterCounterViewController: UIViewController {
-
+    
     @IBOutlet private(set) weak var charactersTypedLabel: UILabel!
     @IBOutlet private(set) weak var charactersRemainingLabel: UILabel!
-
+    
     @IBOutlet private(set) weak var tweetTextView: UITextView!
     
     @IBOutlet private(set) weak var copyTextButton: UIButton!
     @IBOutlet private(set) weak var clearTextButton: UIButton!
     
     @IBOutlet private(set) weak var postTweetButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tweetTextView.delegate = self
+        updateCharacterCount()
     }
+    
+    func updateCharacterCount() {
+        let tweetText = tweetTextView.text ?? ""
+        let characterCount = tweetText.count
+        let remainingCount = 280 - characterCount
+        charactersTypedLabel.text = "\(characterCount)\\280"
+        charactersRemainingLabel.text = "\(remainingCount)"
+    }
+}
 
-
+extension TwitterCounterViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        updateCharacterCount()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = textView.text as NSString
+        let newText = currentText.replacingCharacters(in: range, with: text)
+        return newText.count <= 280
+    }
 }
 
