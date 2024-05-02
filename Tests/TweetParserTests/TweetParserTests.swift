0ac0a23f04231e99c3e57e3e1164ff8060dacbb1
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-05-02 14:39:48 +0300

    Clear Text resets TextView Properties

--- Tests/TweetParserTests/TweetParserTests.swift
+++ Tests/TweetParserTests/TweetParserTests.swift
@@ -1,12 +1,39 @@
 import XCTest
+import Combine
 @testable import TweetParser
 
-final class TweetParserTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+final class TweetTextViewTests: XCTestCase {
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    var textView: TweetTextView!
+    var cancellables: Set<AnyCancellable> = []
+
+    override func setUp() {
+        super.setUp()
+        let dummyCoder = NSKeyedUnarchiver(forReadingWith: Data())
+        guard let tv = TweetTextView(coder: dummyCoder) else {
+            XCTFail("Failed to initialize TweetTextView in setUp()")
+            return
+        }
+        textView = tv
+        textView.text = "Initial text"
+    }
+
+    override func tearDown() {
+        textView = nil
+        cancellables.removeAll()
+        super.tearDown()
+    }
+
+    func testClearTextResetsTextViewProperties() {
+        guard let tv = textView else {
+            XCTFail("TweetTextView instance is nil in testClearText()")
+            return
+        }
+
+        tv.clearText()
+        XCTAssertEqual(tv.text, "")
+        XCTAssertEqual(tv.tweetText, "")
+        XCTAssertEqual(tv.characterCount, 0)
+        XCTAssertEqual(tv.remainingCount, 280)
     }
 }
