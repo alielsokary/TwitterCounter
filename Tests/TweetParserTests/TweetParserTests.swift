c6036e83712632d3f50585af5a97b090cf1aeb25
Ali Elsokary <ali.elsokary.w@gmail.com>
2024-05-02 18:30:09 +0300

    Create makeSut helper function

--- Tests/TweetParserTests/TweetParserTests.swift
+++ Tests/TweetParserTests/TweetParserTests.swift
@@ -4,57 +4,52 @@ import Combine
 
 final class TweetTextViewTests: XCTestCase {
     
-    var textView: TweetTextView!
     var cancellables: Set<AnyCancellable> = []
     
     override func setUp() {
         super.setUp()
-        let dummyCoder = NSKeyedUnarchiver(forReadingWith: Data())
-        guard let tv = TweetTextView(coder: dummyCoder) else {
-            XCTFail("Failed to initialize TweetTextView in setUp()")
-            return
-        }
-        textView = tv
-        textView.text = "Initial text"
+
     }
     
     override func tearDown() {
-        textView = nil
         cancellables.removeAll()
         super.tearDown()
     }
     
     func testClearTextResetsTextViewProperties() {
-        textView.clearText()
-        XCTAssertEqual(textView.text, "")
-        XCTAssertEqual(textView.tweetText, "")
-        XCTAssertEqual(textView.characterCount, 0)
-        XCTAssertEqual(textView.remainingCount, 280)
+        let sut = makeSUT()
+        sut.clearText()
+        XCTAssertEqual(sut.text, "")
+        XCTAssertEqual(sut.tweetText, "")
+        XCTAssertEqual(sut.characterCount, 0)
+        XCTAssertEqual(sut.remainingCount, 280)
     }
     
     func testTextChangeUpdatesCharactersCount() {
+        let sut = makeSUT()
         let expectation = XCTestExpectation(description: "Character count updated")
         let tweetText = "New text"
         
-        let cancellable = textView.$characterCount
+        let cancellable = sut.$characterCount
             .sink { count in
                 if count > 0 {
                     expectation.fulfill()
                 }
             }
         
-        textView.tweetText = tweetText
+        sut.tweetText = tweetText
         
         wait(for: [expectation], timeout: 1.0)
-        XCTAssertEqual(textView.characterCount, 8)
+        XCTAssertEqual(sut.characterCount, 8)
         cancellable.cancel()
     }
     
     func testTextChangeUpdatesRemainingCount() {
+        let sut = makeSUT()
         let expectation = XCTestExpectation(description: "Character count updated")
         let tweetText = "New text"
         
-        let cancellable = textView.$characterCount
+        let cancellable = sut.$characterCount
             .dropFirst()
             .sink { count in
                 if count > 0 {
@@ -62,18 +57,19 @@ final class TweetTextViewTests: XCTestCase {
                 }
             }
         
-        textView.tweetText = tweetText
+        sut.tweetText = tweetText
         
         wait(for: [expectation], timeout: 1.0)
-        XCTAssertEqual(textView.remainingCount, 272)
+        XCTAssertEqual(sut.remainingCount, 272)
         cancellable.cancel()
     }
     
     func testTextChangeWithSpecialCharacterUpdatesCountCorrectly() {
+        let sut = makeSUT()
         let expectation = XCTestExpectation(description: "Character count updated")
         let tweetText = "℞"
         
-        let cancellable = textView.$characterCount
+        let cancellable = sut.$characterCount
             .dropFirst()
             .sink { count in
                 if count > 0 {
@@ -81,18 +77,19 @@ final class TweetTextViewTests: XCTestCase {
                 }
             }
         
-        textView.tweetText = tweetText
+        sut.tweetText = tweetText
         
         wait(for: [expectation], timeout: 1.0)
-        XCTAssertEqual(textView.characterCount, 2)
+        XCTAssertEqual(sut.characterCount, 2)
         cancellable.cancel()
     }
     
     func testTextChangeWithEmojiCharactersUpdatesCountCorrectly() {
+        let sut = makeSUT()
         let expectation = XCTestExpectation(description: "Character count updated")
         let tweetText = "☺️☺️"
         
-        let cancellable = textView.$characterCount
+        let cancellable = sut.$characterCount
             .dropFirst()
             .sink { count in
                 if count > 0 {
@@ -100,11 +97,27 @@ final class TweetTextViewTests: XCTestCase {
                 }
             }
         
-        textView.tweetText = tweetText
+        sut.tweetText = tweetText
         
         wait(for: [expectation], timeout: 1.0)
-        XCTAssertEqual(textView.characterCount, 8)
+        XCTAssertEqual(sut.characterCount, 8)
         cancellable.cancel()
     }
     
+}
+
+// MARK: - Helpers
+private extension TweetTextViewTests {
+    func makeSUT() -> TweetTextView {
+        let textView = TweetTextView(coder: mockCoder())!
+        textView.text = "Initial text"
+        return textView
+    }
+    
+    func mockCoder() -> NSKeyedUnarchiver {
+        let object = String()
+        let data = try! NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false)
+        let coder = try! NSKeyedUnarchiver(forReadingFrom: data)
+        return coder
+    }
 }
